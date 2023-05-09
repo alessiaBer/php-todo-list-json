e4f82c8942d6bc2c1b443edbae93e7ea3771e307
alessiaBer <alexdeyn@gmail.com>
2023-05-09 12:15:59 +0200

    [try] refactoring

--- getTasks.php
+++ getTasks.php
@@ -1,5 +1,7 @@
 <?php 
 //read tasks json file
 $tasksJsonString = file_get_contents('tasks.json');
+// send a FormData() object through axios 
+header('Content-Type: application/json');
 //print array
 echo $tasksJsonString;
--- toggleDone.php
+++ toggleDone.php
@@ -1,5 +1,4 @@
 <?php 
-
 if(isset($_POST["index"])) {
     //assign to a variable the function param
     $index = intval($_POST["index"]);
@@ -11,21 +10,15 @@ if(isset($_POST["index"])) {
     $numericIndexedArray = array_values($tasksArray);
     //assign to a variable the position that have to change
     $task = $numericIndexedArray[$index];
-    // through an if statement verify the value of done of the task and change it when is clicked
-    if($task["done"] === true) {
-        $task["done"] = false;
-    } else {
-        $task["done"] = true;
-    }
-    //assign to a var the task that has to replace the old task
-    $replacement = array($index => $task);
-    //replace values
-    $newTasksArray = array_replace($tasksArray, $replacement);
-     //transform back the string into a json
-    $newTasksJSONString = json_encode($newTasksArray);
+    
+    $task["done"] = !$task["done"];
+
+
+    //transform back the string into a json
+    $newTasksJSONString = json_encode($tasksArray);
     //replace the file content
     file_put_contents('tasks.json', $newTasksJSONString);
-    // send a FormData() object through axios 
+    // send a FormData() object through axios
     header('Content-Type: application/json');
     //print the result json array
     echo $newTasksJSONString;
